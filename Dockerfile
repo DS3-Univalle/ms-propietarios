4c41d768db60a20268ccd063b5c353c5e149fc8b
Brayan J. Gomez M <94873455+BrianGomezM@users.noreply.github.com>
2023-10-31 16:09:41 -0500

    propietarios-v3

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 # Etapa 1: Compilar el proyecto y generar el JAR
-FROM maven:3.8.4-openjdk-17-slim as build
-WORKDIR /build
-COPY pom.xml .
-COPY src ./src
-RUN mvn clean package
+#FROM maven:3.8.4-openjdk-17-slim as build
+#WORKDIR /build
+#COPY pom.xml .
+#COPY src ./src
+#RUN mvn clean package
 
 # Etapa 2: Crear la imagen final
 FROM openjdk:17-jdk-alpine
--- src/main/java/com/proyectocabin/proyectojpa/ProyectoJpaApplication.java
+++ src/main/java/com/proyectocabin/proyectojpa/ProyectoJpaApplication.java
@@ -2,12 +2,16 @@ package com.proyectocabin.proyectojpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class ProyectoJpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProyectoJpaApplication.class, args);
+		SpringApplication app = new SpringApplication(ProyectoJpaApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		System.out.println("Server Port: " + env.getProperty("server.port"));
+
 
 	}
 
